04fc8b4db29ab8eb169f110daa3647cbb65a5717
Justin Roper <jdroper@neo.rr.com>
2016-03-15 23:21:25 -0400

    Update current_user to be article creater & restrict actions to only logged in users

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only:[:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   def new
     @article = Article.new
   end
@@ -9,7 +11,7 @@ class ArticlesController < ApplicationController
     #render plain: params[:article].inspect
     
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
@@ -53,5 +55,12 @@ class ArticlesController < ApplicationController
   end
   def set_article
     @article = Article.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @article.user
+      flash[:danger] = "You can only edit or delete your own articles"
+      redirect_to root_path
+    end
   end
 end
